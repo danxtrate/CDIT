6aba97197e97882a3057ceed79258fde11bcabeb
tatallor <tatallor@gmail.com>
2018-11-20 09:32:12 +0200

    modified:   instance.tf

--- instance.tf
+++ instance.tf
@@ -16,3 +16,22 @@ resource "oci_core_instance" "WebServerA" {
     source_id   = "${var.InstanceImageOCID}"
   }
 }
+
+resource "oci_core_instance" "WebServerB" {
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "WebServerB"
+  shape               = "${var.InstanceShape}"
+  subnet_id           = "${oci_core_subnet.WBB.id}"
+ # hostname_label      = "WebServerA"
+
+#use this to install httpd role
+  metadata {
+    user_data = "${base64encode(var.user-data)}"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = "${var.InstanceImageOCID}"
+  }
+}
