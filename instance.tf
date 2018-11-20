ae62182647c15c6423d0ef1ef3284a616648f679
tatallor <tatallor@gmail.com>
2018-11-20 09:41:32 +0200

    modified:   instance.tf

--- instance.tf
+++ instance.tf
@@ -18,12 +18,12 @@ resource "oci_core_instance" "WebServerA" {
 }
 
 resource "oci_core_instance" "WebServerB" {
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0],"name")}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[1],"name")}"
   compartment_id      = "${var.compartment_ocid}"
   display_name        = "WebServerB"
   shape               = "${var.InstanceShape}"
   subnet_id           = "${oci_core_subnet.WBB.id}"
- # hostname_label      = "WebServerA"
+ # hostname_label      = "WebServerB"
 
 #use this to install httpd role
   metadata {
